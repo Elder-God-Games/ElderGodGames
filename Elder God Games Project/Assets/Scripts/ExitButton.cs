4ff999c71fbdbbe99c2e4c1dd0ba1c9f5e2d2469
S00171657 <S00171657@mail.itsligo.ie>
2018-12-11 19:28:45 +0000

    Audio sliders finished

--- Elder God Games Project/Assets/Scripts/ExitButton.cs
+++ Elder God Games Project/Assets/Scripts/ExitButton.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class ExitButton : MonoBehaviour, IPointerClickHandler
 {
@@ -11,5 +14,9 @@ public class ExitButton : MonoBehaviour, IPointerClickHandler
         Debug.Log("Clicked Exit");
 
         Application.Quit();
+
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#endif
     }
 }
