0719917e884cb2016a81f02417d7a84cdcb35e6e
S00171657 <S00171657@mail.itsligo.ie>
2019-02-02 21:39:36 +0000

    Basic Tower Eye Detection

--- Elder God Games Project/Assets/Scripts/CameraScript.cs
+++ Elder God Games Project/Assets/Scripts/CameraScript.cs
@@ -8,22 +8,24 @@ public class CameraScript : MonoBehaviour {
     public GameObject player;
     private float halfx;
     private float halfy;
+    private float yPos, zPos;
+    [Range(1, 10)]
+    public float lookAhead;
 
 
     // Use this for initialization
     void Start() {
-
+        yPos = transform.position.y;
+        zPos = transform.position.z;
     }
 
     // Update is called once per frame
     void Update() {
-        
-            FollowPlayer();
-
+        FollowPlayer();
     }
     void FollowPlayer(){
         
-        this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, this.transform.position.z);
+        transform.position = new Vector3(player.transform.position.x + lookAhead, yPos, zPos);
 
     }
 }
--- Elder God Games Project/Assets/Scripts/TargetScript.cs
+++ Elder God Games Project/Assets/Scripts/TargetScript.cs
@@ -9,22 +9,48 @@ public class TargetScript : MonoBehaviour {
     private float min, max;
     public float travelDistance = 6;
     public float speed = 3;
+    [Range(0, 10)]
+    public float enemySpawn;
+    public GameObject TowerEye, Player, Enemy;
+    enum State { Idle, Attack}
+    State Eye;
+    public bool isDetected;
 
     // Use this for initialization
     void Start()
     {
         min = this.transform.position.x - (travelDistance / 2);
         max = this.transform.position.x + (travelDistance / 2);
+        Eye = State.Idle;
+        isDetected = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            Eye = State.Attack;
+            Debug.Log("Attack");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        UpdateMovement();
+        if(Eye == State.Idle)
+        {
+            UpdateMovement();
+        }
+        else if(Eye == State.Attack)
+        {
+            AttackStage();
+        }
     }
 
     void UpdateMovement()
     {
+        TowerEye.transform.LookAt(transform);
+
         if (this.transform.position.x > max)
         {
             MovingDirection = Vector3.left * speed;
@@ -39,4 +65,18 @@ public class TargetScript : MonoBehaviour {
         }
         this.transform.Translate(MovingDirection * Time.smoothDeltaTime);
     }
+
+    void AttackStage()
+    {
+        TowerEye.transform.LookAt(Player.transform);
+        transform.position = Player.transform.position;
+        StartCoroutine("waitSpawn");
+    }
+
+    IEnumerator waitSpawn()
+    {
+        yield return new WaitForSeconds(2.0f);
+        Instantiate(Enemy, new Vector3(Player.transform.position.x, 5, 0), Quaternion.identity);
+    }
 }
+
