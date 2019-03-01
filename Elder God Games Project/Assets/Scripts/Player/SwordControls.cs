88e020ab3c52212be87dc893e8aba4fa3f741aa2
William McLaughlin <s00168764@mail.itsligo.ie>
2019-03-01 13:22:42 +0000

    Arranged Folders and fixed platforming
    Arranged scripts into seperate folders.
    Platforming would sometimes result in a superjump if performed from a sloped surface. By settin the player's velocity to 0,0 rith before a jump, the superjump no longer appears to occur

---
+++ Elder God Games Project/Assets/Scripts/Player/SwordControls.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordControls : MonoBehaviour {
+
+    private CapsuleCollider2D weapon;
+    float timer;
+
+	// Use this for initialization
+	void Start () {
+        weapon = GetComponent<CapsuleCollider2D>();
+
+        // time until weapon turns itself off
+        timer = 6f;
+
+        this.gameObject.SetActive(false);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // decrement timer variable by one every update
+        timer--;
+
+        // if the timer variable reaches 0
+        if (timer == 0)
+        {
+            // reset timer
+            // set object to inactive
+            timer = 6f;
+            this.gameObject.SetActive(false);
+        }
+	}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // if the collision is with an object timed enemy
+        if (collision.gameObject.tag == "Enemy")
+        {
+            // destroy the enemy
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Elder God Games Project/Assets/Scripts/Properties.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Properties {
+
+    public float Health { get; set; }
+
+    public int Score { get; set; }
+
+    public float Damage { get; set; }
+}
